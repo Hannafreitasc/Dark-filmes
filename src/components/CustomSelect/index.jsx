a7eff0b26ca2bf9a8514cb8821071d3e8c866bce
hannafreitasc <hannafreitascc@gmail.com>
2025-05-06 12:02:45 -0300

    dark-filmes

---
+++ src/components/CustomSelect/index.jsx
@@ -0,0 +1,18 @@
+export default function CustomSelect({label, options}){
+    return (
+        <div className="w-full flex flex-col gap-2">
+            <label className="text-[17px] font-bold">{label}</label>
+            <select className="w-full h-[45px] bg-[#141414] rounded-lg border border-[#ffffff1a] outline-none focus:border-purple-400 px-2">
+                <option value="">Selecione...</option>
+                {options.map((opcao) => {
+                    return ( 
+                     <option 
+                        key={opcao} 
+                        value={opcao}
+                    >{opcao}</option>
+                    )
+                })}
+            </select>
+        </div>
+    )
+}
